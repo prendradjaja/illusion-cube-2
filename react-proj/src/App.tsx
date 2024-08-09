5106e34c7fc79dec0ecf7973210e72a71f27bd2c
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-08-09 12:01:50 -0700

    Add the buttons

--- react-proj/src/App.tsx
+++ react-proj/src/App.tsx
@@ -3,7 +3,55 @@ import './App.css';
 function App() {
   return (
     <>
-      Hello, world!
+      <div id="cubes-container"></div>
+      <div id="controls">
+        <div>
+          <button className="spacer">-</button
+          ><button >U</button
+          ><button className="spacer">-</button>
+          <br />
+          <button  >L</button
+          ><button >F</button
+          ><button >R</button>
+          <br />
+          <button className="spacer">-</button
+          ><button >D</button
+          ><button >B</button>
+          <br />
+          <br />
+          <br />
+          <button  >M</button
+          ><button >E</button
+          ><button >S</button>
+          <br />
+          <button  >x</button
+          ><button >y</button
+          ><button >z</button>
+        </div>
+        <div>
+          <button className="spacer">-</button
+          ><button >U</button
+          ><button className="spacer">-</button>
+          <br />
+          <button  >L</button
+          ><button >F</button
+          ><button >R</button>
+          <br />
+          <button className="spacer">-</button
+          ><button >D</button
+          ><button >B</button>
+          <br />
+          <br />
+          <br />
+          <button  >M</button
+          ><button >E</button
+          ><button >S</button>
+          <br />
+          <button  >x</button
+          ><button >y</button
+          ><button >z</button>
+        </div>
+      </div>
     </>
   );
 }
