81b196553131af73e1f8c122ebc9cfa1a02910b0
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-08-09 23:46:41 -0700

    delete vanilla project, update react readme

--- src/debug-tools.ts
+++
@@ -1,7 +0,0 @@
-export function DEBUG_getPrivate<T>(obj: any, propName: string): T {
-  return obj[propName]
-}
-
-export function DEBUG_callPrivate<R = void>(obj: any, methodName: string, ...args: any[]): R {
-  return obj[methodName](...args);
-}
