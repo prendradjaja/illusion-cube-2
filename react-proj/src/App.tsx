3a625ed8ffe0c198440b1151d56cd55984cd35c8
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-08-09 20:22:32 -0700

    Yay, JSX strips out those spaces so we can do this easier-to-read version

--- react-proj/src/App.tsx
+++ react-proj/src/App.tsx
@@ -106,50 +106,50 @@ function App() {
       </div>
       <div id="controls">
         <div>
-          <button className="spacer">-</button
-          ><button onMouseDown={(event) => handleClick(1, 'U', event)}>U</button
-          ><button className="spacer">-</button>
+          <button className="spacer">-</button>
+          <button onMouseDown={(event) => handleClick(1, "U", event)}>U</button>
+          <button className="spacer">-</button>
           <br />
-          <button  onMouseDown={(event) => handleClick(1, 'L', event)}>L</button
-          ><button onMouseDown={(event) => handleClick(1, 'F', event)}>F</button
-          ><button onMouseDown={(event) => handleClick(1, 'R', event)}>R</button>
+          <button onMouseDown={(event) => handleClick(1, "L", event)}>L</button>
+          <button onMouseDown={(event) => handleClick(1, "F", event)}>F</button>
+          <button onMouseDown={(event) => handleClick(1, "R", event)}>R</button>
           <br />
-          <button className="spacer">-</button
-          ><button onMouseDown={(event) => handleClick(1, 'D', event)}>D</button
-          ><button onMouseDown={(event) => handleClick(1, 'B', event)}>B</button>
+          <button className="spacer">-</button>
+          <button onMouseDown={(event) => handleClick(1, "D", event)}>D</button>
+          <button onMouseDown={(event) => handleClick(1, "B", event)}>B</button>
           <br />
           <br />
           <br />
-          <button  onMouseDown={(event) => handleClick(1, 'M', event)}>M</button
-          ><button onMouseDown={(event) => handleClick(1, 'E', event)}>E</button
-          ><button onMouseDown={(event) => handleClick(1, 'S', event)}>S</button>
+          <button onMouseDown={(event) => handleClick(1, "M", event)}>M</button>
+          <button onMouseDown={(event) => handleClick(1, "E", event)}>E</button>
+          <button onMouseDown={(event) => handleClick(1, "S", event)}>S</button>
           <br />
-          <button  onMouseDown={(event) => handleClick(1, 'x', event)}>x</button
-          ><button onMouseDown={(event) => handleClick(1, 'y', event)}>y</button
-          ><button onMouseDown={(event) => handleClick(1, 'z', event)}>z</button>
+          <button onMouseDown={(event) => handleClick(1, "x", event)}>x</button>
+          <button onMouseDown={(event) => handleClick(1, "y", event)}>y</button>
+          <button onMouseDown={(event) => handleClick(1, "z", event)}>z</button>
         </div>
         <div>
-          <button className="spacer">-</button
-          ><button onMouseDown={(event) => handleClick(2, 'U', event)}>U</button
-          ><button className="spacer">-</button>
+          <button className="spacer">-</button>
+          <button onMouseDown={(event) => handleClick(2, "U", event)}>U</button>
+          <button className="spacer">-</button>
           <br />
-          <button  onMouseDown={(event) => handleClick(2, 'L', event)}>L</button
-          ><button onMouseDown={(event) => handleClick(2, 'F', event)}>F</button
-          ><button onMouseDown={(event) => handleClick(2, 'R', event)}>R</button>
+          <button onMouseDown={(event) => handleClick(2, "L", event)}>L</button>
+          <button onMouseDown={(event) => handleClick(2, "F", event)}>F</button>
+          <button onMouseDown={(event) => handleClick(2, "R", event)}>R</button>
           <br />
-          <button className="spacer">-</button
-          ><button onMouseDown={(event) => handleClick(2, 'D', event)}>D</button
-          ><button onMouseDown={(event) => handleClick(2, 'B', event)}>B</button>
+          <button className="spacer">-</button>
+          <button onMouseDown={(event) => handleClick(2, "D", event)}>D</button>
+          <button onMouseDown={(event) => handleClick(2, "B", event)}>B</button>
           <br />
           <br />
           <br />
-          <button  onMouseDown={(event) => handleClick(2, 'M', event)}>M</button
-          ><button onMouseDown={(event) => handleClick(2, 'E', event)}>E</button
-          ><button onMouseDown={(event) => handleClick(2, 'S', event)}>S</button>
+          <button onMouseDown={(event) => handleClick(2, "M", event)}>M</button>
+          <button onMouseDown={(event) => handleClick(2, "E", event)}>E</button>
+          <button onMouseDown={(event) => handleClick(2, "S", event)}>S</button>
           <br />
-          <button  onMouseDown={(event) => handleClick(2, 'x', event)}>x</button
-          ><button onMouseDown={(event) => handleClick(2, 'y', event)}>y</button
-          ><button onMouseDown={(event) => handleClick(2, 'z', event)}>z</button>
+          <button onMouseDown={(event) => handleClick(2, "x", event)}>x</button>
+          <button onMouseDown={(event) => handleClick(2, "y", event)}>y</button>
+          <button onMouseDown={(event) => handleClick(2, "z", event)}>z</button>
         </div>
       </div>
     </>
